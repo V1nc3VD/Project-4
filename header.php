a3c94e2117bb3a6ccbddda8059bc1bc4113b7dfc
Vince van Doorn <328388@student.mboutrecht.nl>
2020-06-11 12:51:36 +0200

    header en navbar minder crack

--- header.php
+++ header.php
@@ -1,1 +1,1 @@
-<div class="col-12"><img src="./img/posche.jpg" class="bannerfoto" alt="pyjamabanner"></div>
+<img src="./img/posche.jpg" class="bannerfoto" alt="pyjamabanner">
--- navigation.php
+++ navigation.php
@@ -1,7 +1,21 @@
-<div class="col-12" id="navigation">
-  <ul class="nav nav-tabs">
-    <li class="nav-item">
-      <a class="nav-link" href="./index.php?content=home">Home</a>
-    </li>
-  </ul>
-</div>
+<nav class="navbar navbar-expand-sm navbar-light bg-light col-12">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pyjamas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Navbar text with an inline element
+    </span>
+  </div>
+</nav>
