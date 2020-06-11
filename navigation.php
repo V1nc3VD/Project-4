e010219ad6c0c39fa6c0f358a5d7d58bf23a37f3
Aron van de Ridder <aronridder@hotmail.nl>
2020-06-11 13:03:02 +0200

    nav

--- navigation.php
+++ navigation.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <nav class="navbar navbar-expand-sm navbar-light bg-light col-12">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -20,17 +20,4 @@
     </span>
   </div>
 </nav>
-=======
-<<<<<<< HEAD
-<div class="col-12"><img src="./img/posche.jpg"  width="100%" alt=""></div>
 
-=======
->>>>>>> 66d8a9617f7c691d36fa8d4c7ddcdfab9cf10a6e
-<div class="col-12" id="navigation">
-  <ul class="nav nav-tabs">
-    <li class="nav-item">
-      <a class="nav-link" href="./index.php?content=home">Home</a>
-    </li>
-  </ul>
-</div>
->>>>>>> ea3a58667fe9ba7e71e5da6a9267efca9bc1fc73
