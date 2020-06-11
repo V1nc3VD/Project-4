761ffc8bd2a7ffb114cda3b105a5105f060a4e81
koos van losenoord <316595@student.mboutrecht.nl>
2020-06-11 13:48:21 +0200

    background

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 <div class="col-12 footer">
    <div id="text"><h5>© Ninja in Pyjama</h5></div>
-   <div id="text2">daltonlaan 300</div>
+   <div id="text2">Adres: Daltonlaan 300 <br>
+   Tel: 0610040543 <br>
+   Postcode: 3445jk <br>
+   Email: Ninjasinpyjama@gmail.com
+   </div>
 
 </div>
