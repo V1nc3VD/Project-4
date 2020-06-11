fda1348ea169815466c0ef1ea20def5628e9fb2d
koos van losenoord <316595@student.mboutrecht.nl>
2020-06-11 13:53:23 +0200

    shop

--- navigation.php
+++ navigation.php
@@ -14,6 +14,9 @@
       <li class="nav-item">
         <a class="nav-link" href="./index.php?content=cantact">Contact</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./index.php?content=shop">shop</a>
+      </li>
     </ul>
     <span class="navbar-text">
       Navbar text with an inline element
