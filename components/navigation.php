063de3ccf7d40bac52291540f020ba3ac2b2a8e8
Vince van Doorn <328388@student.mboutrecht.nl>
2020-06-11 22:13:55 +0200

    navbar profiel icoontje

--- components/navigation.php
+++ components/navigation.php
@@ -1,4 +1,3 @@
-
 <nav class="navbar navbar-expand-sm navbar-light  col-12">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -14,6 +13,9 @@
       <li class="nav-item">
         <a class="nav-link" href="./index.php?content=contact">Contact</a>
       </li>
-    </ul>
+      <div class="account ml-auto">
+      <?php include("./components/profielbutton.php") ?>
   </div>
+  </div>
+  </ul>
 </nav>
---
+++ components/profielbutton.php
@@ -0,0 +1,12 @@
+<!--bij kleine schermgrootte zie je dit wanneer je niet bent ingelogd-->
+<div class="dropdown profielklein">
+    <a href="dropdown-toggle" class="btn btn-light " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <img src="./img/profile.png" class="profielklein" alt="inloggen/registreren">
+    </a>
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="./index.php?content=Inloggen">Inloggen</a>
+        <a class="dropdown-item" href="./index.php?content=register">Account aanmaken</a>
+    </div>
+</div>
+<!-- profiel? -->
+</div>
