01428a185c622d22dc099ae9bd0b20741f72a635
koos van losenoord <316595@student.mboutrecht.nl>
2020-06-11 13:27:34 +0200

    font

--- footer.php
+++ footer.php
@@ -1,1 +1,5 @@
-<div class="col-12 footer">© Ninja in Pyjama <br></div>
+<div class="col-12 footer">
+   <div id="text"><h5>© Ninja in Pyjama</h5></div>
+   <div id="text2">daltonlaan 300</div>
+
+</div>
--- index.php
+++ index.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="./css/style.css">
   <link href="https://fonts.googleapis.com/css?family=Italianno&display=swap" rel="stylesheet">
   <title>Big PP</title>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
 </head>
 
 <body>
