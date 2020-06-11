eb0cf1440454a3853d5ae4c91744ea576f1f7522
koos van losenoord <316595@student.mboutrecht.nl>
2020-06-11 12:02:02 +0200

    first commit

---
+++ navigation.php
@@ -0,0 +1,9 @@
+<div class="col-12"><img src="./img/posche.jpg"  alt=""></div>
+
+<div class="col-12" id="navigation">
+  <ul class="nav nav-tabs">
+    <li class="nav-item">
+      <a class="nav-link" href="./index.php?content=home">Home</a>
+    </li>
+  </ul>
+</div>
