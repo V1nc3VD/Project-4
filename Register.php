cc70e81f7a09ee92121c1e589001cfc008019f3a
koos van losenoord <316595@student.mboutrecht.nl>
2020-06-18 09:26:44 +0200

    fuck3

---
+++ Register.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="row justify-content-center align-items-center" style="height:70vh">
+        <div class="col-12 col-sm-5">
+            <div class="card">
+                <div class="card-body card-color">
+                    <form action="./index.php?content=register_script" method="post">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" name="email" class="form-control" require id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            <small id="emailHelp" class="form-text ">We'll never share your email with anyone else.</small>
+                        </div>
+                        <button type="submit" id="sendlogin" class="btn btn-primary">login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ inloggen.php
@@ -0,0 +1,21 @@
+<div class="container">
+        <div class="row justify-content-center align-items-center" style="height:100vh">
+            <div class="col-5">
+                <div class="card">
+                    <div class="card-body card-color">
+                        <form action="./phpscript/inloggen_script.php" autocomplete="off">
+                            <div class="form-group">
+                                Username
+                                <input type="text" class="form-control" name="username">
+                            </div>
+                            <div class="form-group">
+                                Password
+                                <input type="password" class="form-control" name="password">
+                            </div>
+                            <button type="submit" id="sendlogin" class="btn btn-primary">login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
