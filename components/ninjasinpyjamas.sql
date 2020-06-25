d45018380abfe5d99c12f72b0a43d3c67c2ec7bd
Vince van Doorn <328388@student.mboutrecht.nl>
2020-06-26 01:33:50 +0200

    database bijgewerkt (zet de sqlcode in phpmyadmin)

--- components/ninjasinpyjamas.sql
+++ components/ninjasinpyjamas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 17 jun 2020 om 22:16
--- Serverversie: 10.4.10-MariaDB
--- PHP-versie: 7.3.12
+-- Gegenereerd op: 25 jun 2020 om 23:32
+-- Serverversie: 5.7.26
+-- PHP-versie: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `pyjamas`
+--
+
+DROP TABLE IF EXISTS `pyjamas`;
+CREATE TABLE IF NOT EXISTS `pyjamas` (
+  `PyjamaID` int(11) NOT NULL AUTO_INCREMENT,
+  `Naam` varchar(50) NOT NULL,
+  `Foto` varchar(100) NOT NULL,
+  `Beschrijving` varchar(200) DEFAULT NULL,
+  `Prijs` decimal(5,2) DEFAULT NULL,
+  `Voorraad` int(11) DEFAULT NULL,
+  PRIMARY KEY (`PyjamaID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `pyjamas`
+--
+
+INSERT INTO `pyjamas` (`PyjamaID`, `Naam`, `Foto`, `Beschrijving`, `Prijs`, `Voorraad`) VALUES
+(1, 'Raccoon', 'raccoon', 'Net als de enige echte RaccoonEggs Yeet!', '24.99', 100),
+(2, 'Bunny', 'bunny', 'Dit mooie konijnekostuum is een must have!.', '200.00', 120),
+(5, 'Squirtle', 'squirtle', 'Deze Onesie is een echte must have voor pokemon fans', '24.99', 8000),
+(6, 'Mascotte!!', 'hauto', 'Onze enige echte Hauto nu te koop voor een prikkie!', '999.99', 12);
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `users`
 --
 
@@ -33,17 +60,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `USER_ID` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
   `password` varchar(60) NOT NULL,
-  `nickname` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
   `userrole` enum('root','admin','support','customer') NOT NULL DEFAULT 'customer',
   PRIMARY KEY (`USER_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `users`
 --
 
-INSERT INTO `users` (`USER_ID`, `email`, `password`, `nickname`, `userrole`) VALUES
-(1, 'vincevandoorn@gmail.com', '$2y$10$Shm/nDnyWbO9CttAES/WBOUULLzLICOqryqWD6ji9jBUYJI8s.h9S', 'Vince van Doorn', 'customer');
+INSERT INTO `users` (`USER_ID`, `email`, `password`, `name`, `userrole`) VALUES
+(1, 'vincevandoorn@gmail.com', '$2y$10$Shm/nDnyWbO9CttAES/WBOUULLzLICOqryqWD6ji9jBUYJI8s.h9S', 'Vince van Doorn', 'admin');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
